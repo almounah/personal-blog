6fe4f973f06b553231b0b705da2382acd97a4aa8
almounah <haroun.al-mounayar@grenoble-inp.org>
2024-02-18 21:54:12 +0100

    Started nav bar with go component

--- pkg/api/serve.go
+++ pkg/api/serve.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -12,10 +13,24 @@ type ServeConfig struct {
 	TLSKeyPath  string
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/templates/index.html", "web/templates/common/navbar.html"))
+    w.Header().Set("Content-type", "text/html")
+	tmpl.Execute(w, nil)
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "image/x-icon")
+	http.ServeFile(w, r, "web/static/icons/favicon.ico")
+}
+
 func Serve(conf ServeConfig) {
+    http.HandleFunc("/favicon.ico", faviconHandler)
+	http.HandleFunc("/", rootHandler)
+	fmt.Println("Will now listen on " + conf.Ip + ":" + conf.Port)
 	err := http.ListenAndServe(conf.Ip+":"+conf.Port, nil)
-    if err != nil {
-        fmt.Println("An error Occured")
-    }
+	if err != nil {
+		fmt.Println("An error Occured")
+	}
 
 }
